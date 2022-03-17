f858ea0111bcdb160c9e93bf3ca51c8623d836db
enigbe <ochekliyeenigbe@gmail.com>
2022-03-17 14:34:32 +0100

    feat: initial project scaffold
    - add necessary files and folders
    - add clip and field modules
    
    [WIP]

---
+++ src/lib/domain/clip/field/mod.rs
@@ -0,0 +1,17 @@
+mod clip_id;
+mod shortcode;
+mod content;
+mod title;
+mod posted;
+mod expires;
+mod password;
+mod hits;
+
+pub use clip_id::ClipId;
+pub use shortcode::ShortCode;
+pub use content::Content;
+pub use title::Title;
+pub use posted::Posted;
+pub use expires::Expires;
+pub use password::Password;
+pub use hits::Hits;
