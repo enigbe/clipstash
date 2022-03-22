f2a2570b8126e7e9f1727a54fdf9be435bbc5987
enigbe <ochekliyeenigbe@gmail.com>
2022-03-22 09:11:03 +0100

    conf: create and export lib modules

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,4 +1,8 @@
 pub mod domain;
 pub mod data;
-// pub mod service;
-// pub mod web;
+pub mod service;
+pub mod web;
+
+pub use domain::clip::field::ShortCode;
+pub use domain::clip::ClipError;
+pub use domain::time::Time;
